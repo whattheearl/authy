77155158d892b11414d72e5720ce333b9a866c57
jonathan earl <jonathan.earl@tylertech.com>
2024-09-02 13:09:18 -0700

    add apps

---
+++ src/routes/apps.ts
@@ -0,0 +1,15 @@
+import Elysia, { t } from "elysia";
+import { getAppById, getAppByName, getApps } from "../db/apps";
+
+export const appsRoute = new Elysia({ prefix: '/apps' })
+  .get('/', () => getApps())
+  .get(
+    '/id/:id',
+    ({ params: { id } }) => getAppById(id),
+    { params: t.Object({ id: t.Number() }) }
+  )
+  .get(
+    '/name/:name',
+    ({ params: { name } }) => getAppByName(name),
+    { params: t.Object({ name: t.String() }) }
+  )
