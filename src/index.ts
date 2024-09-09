e7c0c818ee7c7784385aa1f3f4c31b47bd6e9fb2
jonathan earl <jonathan.earl@tylertech.com>
2024-09-08 18:54:24 -0700

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { clientRoute } from "./routes/clients";
+import swagger from "@elysiajs/swagger";
 
 const app = new Elysia()
+  .use(swagger())
   .use(clientRoute)
   .listen(3000);
 
