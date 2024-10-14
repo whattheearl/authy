57b8b7aa96a96ca622db5af1b1ac50dfd0e9939a
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-13 19:23:02 -0700

    add signout

--- src/routes/signout.ts
+++ src/routes/signout.ts
@@ -1,20 +1,25 @@
-import Elysia, { redirect, t } from "elysia";
+import Elysia, { redirect, t } from 'elysia';
 
-export const signout = new Elysia({ prefix: '/signout' })
-.post('/', ({ cookie: { sess } }) => {
-    console.log(sess);
-    console.log(sess.value)
-    if (sess.value) sess.value = '';
-    return redirect('/')
-},{
-    cookie: t.Optional(
-        t.Cookie({
-            sess: t.Optional(t.String()),
-        },
-        {
-            httpOnly: true,
-            secure: Bun.env.cookie__secure == 'true',
-            secrets: 'dev-secret',
-        })
-    ),
-})
+export const signout = new Elysia({ prefix: 'signout' }).post(
+    '/',
+    ({ cookie: { sess } }) => {
+        console.log(sess);
+        console.log(sess.value);
+        if (sess.value) sess.value = '';
+        return redirect('/');
+    },
+    {
+        cookie: t.Optional(
+            t.Cookie(
+                {
+                    sess: t.Optional(t.String()),
+                },
+                {
+                    httpOnly: true,
+                    secure: Bun.env.cookie__secure == 'true',
+                    secrets: 'dev-secret',
+                },
+            ),
+        ),
+    },
+);
