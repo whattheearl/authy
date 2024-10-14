0b0762793e95cb102c0c7922a3cb867845c189fe
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-13 19:22:24 -0700

    add signin page

--- src/routes/signin/server.ts
+++ src/routes/signin/server.ts
@@ -1,81 +1,105 @@
-import html from "@elysiajs/html";
-import Elysia, { error, redirect, t } from "elysia";
+import html from '@elysiajs/html';
+import Elysia, { error, redirect, t } from 'elysia';
 import page from './page';
-import { getUserByUsername } from "../../lib/users";
+import { getUserByUsername } from '../../lib/users';
 
-export const signin = new Elysia({ prefix: '/'})
-.use(html())
-.get('/', ({ html, cookie: { sess } }) => {
-    if (!sess.value) {
-        return html(page())
-    }
+export const signin = new Elysia({ prefix: '/' })
+    .use(html())
+    .get(
+        '/',
+        ({ html, cookie: { sess } }) => {
+            if (!sess.value) {
+                return html(page());
+            }
 
-    const session = JSON.parse(sess.value ?? '{}');
-    if (!session.username) {
-        return html(page())
-    }
+            const session = JSON.parse(sess.value ?? '{}');
+            if (!session.username) {
+                return html(page());
+            }
 
-    return redirect('/apps');
-}, {
-    cookie: t.Optional(
-        t.Cookie({
-            sess: t.Optional(t.String()),
+            return redirect('/apps');
         },
         {
-            httpOnly: true,
-            secure: Bun.env.cookie__secure == 'true',
-            secrets: 'dev-secret',
-        })
-    ),
-})
-.post('/', async ({ body: { username, password }, cookie: { sess, oauth } }) => {
-    console.log(username, password)
-    // INFO: validate user and create session
-    const user = getUserByUsername(username);
-    console.log({user})
-    if (!user || !user.password || !user.username) return error(404);
-    const isMatch = await Bun.password.verify(password, user.password);
-    console.log(isMatch);
-    if (!isMatch) return error(404)
-    sess.value = JSON.stringify({ username: user.username });
+            cookie: t.Optional(
+                t.Cookie(
+                    {
+                        sess: t.Optional(t.String()),
+                    },
+                    {
+                        httpOnly: true,
+                        secure: Bun.env.cookie__secure == 'true',
+                        secrets: 'dev-secret',
+                    },
+                ),
+            ),
+        },
+    )
+    .post(
+        '/',
+        async ({ body: { username, password }, cookie: { sess, oauth } }) => {
+            console.log(username, password);
+            // INFO: validate user and create session
+            const user = getUserByUsername(username);
+            console.log('user exists', { user });
+            if (!user || !user.password || !user.username) return error(404);
+            const isMatch = await Bun.password.verify(password, user.password);
+            console.log('credentials are matched');
+            if (!isMatch) {
+                console.log('password does not match');
+                return error(404);
+            }
+            const session = { username: user.username };
+            sess.value = JSON.stringify(session);
+            console.log('session created', session);
+            // INFO: send to app page if no previous oauth
+            if (!oauth.value) {
+                console.log('no oauth flow found');
+                return redirect('/apps');
+            }
 
-    // INFO: send to app page if no previous oauth
-    if (!oauth.value) return redirect('/apps')
-
-    // INFO: construct authorization url and send if previously tried to login
-    const oauthParams = JSON.parse(oauth.value) as {
-        client_id: string,
-        scope: string,
-        response_type: string,
-        code_challenge: string,
-        nonce: string,
-        state: string,
-        redirect_uri: string,
-    }
-    const url = new URL('/');
-    url.searchParams.set('client_id', oauthParams.client_id);
-    url.searchParams.set('scope', oauthParams.scope);
-    url.searchParams.set('response_type', oauthParams.response_type);
-    url.searchParams.set('code_challenge', oauthParams.code_challenge);
-    url.searchParams.set('nonce', oauthParams.nonce);
-    url.searchParams.set('state', oauthParams.state);
-    url.searchParams.set('redirect_uri', oauthParams.redirect_uri);
-    oauth.value = '';
-    return redirect(url.toString())
-},{
-    cookie: t.Optional(
-        t.Cookie({
-            sess: t.Optional(t.String()),
-            oauth: t.Optional(t.String()),
+            // INFO: construct authorization url and send if previously tried to login
+            const oauthParams = JSON.parse(oauth.value) as {
+                client_id: string;
+                scope: string;
+                response_type: string;
+                code_challenge: string;
+                nonce: string;
+                state: string;
+                redirect_uri: string;
+            };
+            console.log('oauth flow found', oauthParams);
+            const url = new URL('http://localhost:3000/authorization');
+            url.searchParams.set('client_id', oauthParams.client_id);
+            url.searchParams.set('scope', oauthParams.scope);
+            url.searchParams.set('response_type', oauthParams.response_type);
+            url.searchParams.set('code_challenge', oauthParams.code_challenge);
+            url.searchParams.set('nonce', oauthParams.nonce);
+            url.searchParams.set('state', oauthParams.state);
+            url.searchParams.set('redirect_uri', oauthParams.redirect_uri);
+            console.log('redirecting to ', url.toString());
+            return new Response('redirect', {
+                status: 302,
+                headers: { location: url.toString() },
+            });
         },
         {
-            httpOnly: true,
-            secure: Bun.env.cookie__secure == 'true',
-            secrets: 'dev-secret',
-        })
-    ),
-    body: t.Object({
-        username: t.String(),
-        password: t.String(),
-    })
-})
+            cookie: t.Optional(
+                t.Cookie(
+                    {
+                        sess: t.Optional(t.String()),
+                        oauth: t.Optional(t.String()),
+                    },
+                    {
+                        httpOnly: true,
+                        secure: Bun.env.cookie__secure == 'true',
+                        secrets: 'dev-secret',
+                        sameSite: 'lax',
+                    },
+                ),
+            ),
+            body: t.Object({
+                username: t.String(),
+                password: t.String(),
+            }),
+        },
+    );
