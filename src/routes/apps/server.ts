dd7f8f4d6f7883b5d3439b358b2bb3921cca87d7
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-13 19:22:38 -0700

    add apps page

--- src/routes/apps/server.ts
+++ src/routes/apps/server.ts
@@ -1,37 +1,39 @@
-import html from "@elysiajs/html";
-import Elysia, { redirect, t } from "elysia";
+import html from '@elysiajs/html';
+import Elysia, { redirect, t } from 'elysia';
 import page from './page';
 
-export const apps = new Elysia({ prefix: '/apps'})
-.use(html())
-.get('/', ({ html, cookie: { sess } }) => {
-    console.log('hey');
-    console.log(sess.value);
-    if (!sess.value) {
-        return redirect('/');
-    }
-    const session = JSON.parse(sess.value ?? '{}');
-    console.log({session})
-    if (!session.username) {
-        return redirect('/');
-    }
+export const apps = new Elysia({ prefix: '/apps' }).use(html()).get(
+    '/',
+    ({ html, cookie: { sess } }) => {
+        console.log('hey');
+        console.log(sess.value);
+        if (!sess.value) {
+            return redirect('/');
+        }
+        const session = JSON.parse(sess.value ?? '{}');
+        console.log({ session });
+        if (!session.username) {
+            return redirect('/');
+        }
 
-    return html(
-        page({
-            apps: [
-                {name: 'some app', href: 'http://localhost:5173/'}
-            ]
-        })
-    )
-}, {
-    cookie: t.Optional(
-        t.Cookie({
-            sess: t.Optional(t.String()),
-        },
-        {
-            httpOnly: true,
-            secure: Bun.env.cookie__secure == 'true',
-            secrets: 'dev-secret',
-        })
-    ),
-});
+        return html(
+            page({
+                apps: [{ name: 'some app', href: 'http://localhost:5173/' }],
+            }),
+        );
+    },
+    {
+        cookie: t.Optional(
+            t.Cookie(
+                {
+                    sess: t.Optional(t.String()),
+                },
+                {
+                    httpOnly: true,
+                    secure: Bun.env.cookie__secure == 'true',
+                    secrets: 'dev-secret',
+                },
+            ),
+        ),
+    },
+);
